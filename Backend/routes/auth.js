0cc29fcd1db759678dbbc157503c3feaf3c5d878
MedNkhalaf <medn.khalaf@gmail.com>
2023-12-09 16:33:06 +0100

    updt

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,12 +2,6 @@ import express from "express"
 
 
 const router = express.Router()
-router.get("/", (req, res)=>{
-    res.send("Login")
-})
-router.get("/signup", (req, res)=>{
-    res.send("Signup")
-})
 
 
 export default router
---
+++ Backend/routes/rooms.js
@@ -0,0 +1,19 @@
+import express from "express"
+import Room from "../models/Room.js"
+
+
+const router = express.Router()
+router.post("/", async (req,res)=>{
+
+    const newRoom= new Room(req.body)
+    try{ 
+       const savedRoom= await newRoom.save()
+       res.status(200).json(savedRoom)
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
+
+
+export default router
