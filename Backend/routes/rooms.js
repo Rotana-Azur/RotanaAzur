ac2f142b5f3ccc62fe3e2ec3d0d597912000f9bd
MedNkhalaf <medn.khalaf@gmail.com>
2023-12-19 19:53:05 +0100

    crud

--- Backend/routes/rooms.js
+++ Backend/routes/rooms.js
@@ -14,6 +14,49 @@ router.post("/", async (req,res)=>{
 
     }
 })
+router.put("/:id", async (req,res)=>{
+
+    try{ 
+       const updatedRoom= await Room.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true})
+       res.status(200).json(updatedRoom)
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
+
+router.delete("/:id", async (req,res)=>{
+
+    try{ 
+  await Room.findByIdAndDelete(req.params.id)
+       res.status(200).json(deletedRoom)
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
+
+
+router.get("/:id", async (req,res)=>{
+
+    try{ 
+       const room= await Room.findById(req.params.id)
+       res.status(200).json(room)
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
+router.get("/", async (req,res)=>{
+
+    try{ 
+       const rooms= await Room.find()
+       res.status(200).json(rooms)
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
 
 
 export default router
