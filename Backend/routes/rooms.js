0560344ea17db65a451e28d78829431dadec43bb
MedNkhalaf <medn.khalaf@gmail.com>
2023-12-20 17:41:59 +0100

    controllers

--- Backend/routes/rooms.js
+++ Backend/routes/rooms.js
@@ -1,62 +1,17 @@
 import express from "express"
 import Room from "../models/Room.js"
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/roomController.js"
 
 
 const router = express.Router()
-router.post("/", async (req,res)=>{
+router.post("/", createRoom)
+router.put("/:id", updateRoom)
 
-    const newRoom= new Room(req.body)
-    try{ 
-       const savedRoom= await newRoom.save()
-       res.status(200).json(savedRoom)
-    }catch(err){
-        res.status(500).json(err)
-
-    }
-})
-router.put("/:id", async (req,res)=>{
-
-    try{ 
-       const updatedRoom= await Room.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true})
-       res.status(200).json(updatedRoom)
-    }catch(err){
-        res.status(500).json(err)
-
-    }
-})
-
-router.delete("/:id", async (req,res)=>{
-
-    try{ 
-  await Room.findByIdAndDelete(req.params.id)
-       res.status(200).json(deletedRoom)
-    }catch(err){
-        res.status(500).json(err)
-
-    }
-})
+router.delete("/:id", deleteRoom)
 
 
-router.get("/:id", async (req,res)=>{
-
-    try{ 
-       const room= await Room.findById(req.params.id)
-       res.status(200).json(room)
-    }catch(err){
-        res.status(500).json(err)
-
-    }
-})
-router.get("/", async (req,res)=>{
-
-    try{ 
-       const rooms= await Room.find()
-       res.status(200).json(rooms)
-    }catch(err){
-        res.status(500).json(err)
-
-    }
-})
+router.get("/:id", getRoom)
+router.get("/", getRooms)
 
 
 export default router
