89f6d6803377ded243480e9f179803313562afea
MedNkhalaf <medn.khalaf@gmail.com>
2023-12-26 19:26:35 +0100

    updt1

---
+++ Backend/routes/rooms.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/roomController.js"
+
+
+const router = express.Router()
+router.post("/", createRoom)
+router.put("/:id", updateRoom)
+
+router.delete("/:id", deleteRoom)
+
+
+router.get("/:id", getRoom)
+router.get("/", getRooms)
+
+
+export default router
